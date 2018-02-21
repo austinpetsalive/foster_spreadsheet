b50e1a7228310bfc4492f761461ba86436a35645
Walter Moreira <walter@waltermoreira.net>
2018-02-21 14:57:07 -0600

    Add full updating

--- app/app.py
+++ app/app.py
@@ -56,6 +56,43 @@ def fix_formulas(ws):
     ]
     ws.update_cells('AB3:AB', values)
 
+def new_row(old_row: List, dog: Dict[str, Any], person: Dict[str, Any],
+            apa_id: str, dog_internal_id: str, person_internal_id: str) -> List:
+    return [
+        dog['Name'],
+        apa_id,
+        get_bc_mc(dog),
+        dog['Breed'],
+        get_dob(dog),
+        '',
+        get_fee(dog),
+        dog['Sex'],
+        dog['Altered'],
+        old_row[9],
+        old_row[10],
+        old_row[11],
+        old_row[12],
+        ' '.join([person['Firstname'], person['Lastname']]),
+        person['Email'],
+        old_row[15],
+        get_phone(person),
+        old_row[17],
+        old_row[18],
+        old_row[19],
+        old_row[20],
+        old_row[21],
+        old_row[22],
+        dog['Status'],
+        old_row[24],
+        old_row[25],
+        old_row[26],
+        old_row[27],
+        old_row[28],
+        old_row[29],
+        old_row[30],
+        dog_internal_id,
+        person_internal_id
+    ]
 
 class Foster(object):
 
@@ -120,31 +157,44 @@ class Foster(object):
             }
         return shelterluv.get_people(self.api_key, internal_id)
 
-    def info(self, apa_id: str) -> Dict[str, Any]:
-        dog_id, person_id = self.get_internal_ids(apa_id)
-        dog = self.dog_info(dog_id)
-        person = self.person_info(person_id)
-        return {
-            'Name': dog['Name'],
-            'ID Number': dog['ID'],
-            'BC/MC': get_bc_mc(dog),
-            'Breed': dog['Breed'],
-            'DOB': get_dob(dog),
-            'Sex': dog['Sex'],
-            'Spay / Neuter': dog['Altered'],
-            'Foster Full Name': ' '.join([person['Firstname'], person['Lastname']]),
-            'Email': person['Email'],
-            'Cell Phone': get_phone(person),
-            'Adoption Fee': get_fee(dog),
-            'Status': dog['Status'],
-            'dog_internal_id': dog_id,
-            'person_internal_id': person_id
-        }
+    def refresh_all(self):
+        ws = self.sheet.worksheet_by_title('Tracking')
+        all_values = ws.get_all_values()
+        new_values = []
+        for row in all_values[2:]:
+            apa_id = row[1]
+            if not apa_id:
+                break
+            print(f'Processing {apa_id}')
+            dog_internal_id = row[31]
+            person_internal_id = row[32]
+            if not (dog_internal_id and person_internal_id):
+                print('.. missing some ids, will get them')
+                dog_internal_id, person_internal_id = self.get_internal_ids(apa_id)
+            dog = self.dog_info(dog_internal_id)
+            person = self.person_info(person_internal_id)
+            new_values.append(
+                new_row(row, dog, person, apa_id, dog_internal_id, person_internal_id))
+        print('Updating spreadsheet')
+        ws.update_cells('A3', values=new_values)
+        fix_formulas(ws)
+        timestamp = datetime.datetime.now().strftime('%m/%d/%Y %I:%M% %p')
+        ws.update_cell(
+            'A1',
+            f'Last Full Update: {timestamp}'
+        )
+        # get all values
+        # if value has both internals
+        #    self.dog_info, self.person_info
+        # else
+        #    self.info(apa_id)
+        # update row
+        # update_values
+        # update timestamp
 
     def append_dog(self, apa_id: str) -> None:
         ws = self.sheet.worksheet_by_title('Tracking')
         ids = ws.get_col(2)
-        # row values empty
         values = ['']*32
         if apa_id in ids:
             print(f'Updating dog {apa_id}')
@@ -156,31 +206,12 @@ class Foster(object):
         else:
             print(f'Creating new row for {apa_id}')
         try:
-            info = self.info(apa_id)
-            # insert row values too
+            dog_internal_id, person_internal_id = self.get_internal_ids(apa_id)
+            dog = self.dog_info(dog_internal_id)
+            person = self.person_info(person_internal_id)
             ws.insert_rows(
                 len(ids),
-                values=[[
-                    info['Name'],
-                    apa_id,
-                    info['BC/MC'],
-                    info['Breed'],
-                    info['DOB'],
-                    '=IF(trim(E)="","--",(TODAY()-E)/7)',
-                    info['Adoption Fee'],
-                    info['Sex'],
-                    info['Spay / Neuter'],
-                    values[9], values[10], values[11], values[12],
-                    info['Foster Full Name'],
-                    info['Email'],
-                    values[15],
-                    info['Cell Phone'],
-                    values[17], values[18], values[19], values[20], values[21], values[22],
-                    info['Status'],
-                    values[24], values[25], values[26], values[27], values[28], values[29], values[30],
-                    info['dog_internal_id'],
-                    info['person_internal_id']
-                ]]
+                values=[new_row(values, dog, person, apa_id, dog_internal_id, person_internal_id)]
             )
         except Exception as exc:
             ws.insert_rows(
